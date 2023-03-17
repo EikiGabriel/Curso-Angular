35db79c8d5c8e577eae1d8dcaa76978de9d21979
Gabriel Eiki <yokoyama197@gmail.com>
2023-03-17 13:43:22 -0300

    emitir/interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { ParentDataComponent } from './components/parent-data/parent-data.compon
 import { DirectivesComponent } from './components/directives/directives.component';
 import { IfRenderComponent } from './components/if-render/if-render.component';
 import { EventosComponent } from './components/eventos/eventos.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +19,9 @@ import { EventosComponent } from './components/eventos/eventos.component';
     DirectivesComponent,
     IfRenderComponent,
     EventosComponent,
+    EmitterComponent,
+    ChangeNumberComponent,
+    ListRenderComponent,
   ],
   imports: [
     BrowserModule
